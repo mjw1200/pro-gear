5f15a253989337012a952ff38b44ed9327766731
Mike Waldron <michael.waldron@optimizely.com>
2021-12-10 13:09:17 -0700

    DI work, mostly

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,25 +1,23 @@
-﻿using ConsoleApp1.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
-    using ConsoleApp1.Xml;
-    using System;
-    using System.Xml.Serialization;
+    using ConsoleApp1.DependencyInjection;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using System.Threading.Tasks;
 
     class Program
     {
         static Task Main(string[] args) =>
             CreateHostBuilder(args).Build().RunAsync();
 
-        static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureServices((_, services) =>
                     services.AddHostedService<Worker>()
                             .AddScoped<IMessageWriter, LoggingMessageWriter>()
                             .AddHostedService<Jerker>()
                             .AddScoped<IMjwMessageWriter, MjwMessageWriter>());
+        }
     }
 }
