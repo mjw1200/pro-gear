20d7458a37dced5ae69b1a25cd8bc8c8e296b43a
Mike Waldron <91098610+mjw-optimizely@users.noreply.github.com>
2022-02-28 09:36:54 -0700

    Clean up tools scripts

--- tools/devtools.ps1
+++
@@ -1,5 +0,0 @@
-Start-IISCommitDelay
-Remove-IISSite -Name "DevTools" -Confirm:$false
-$site = New-IISSite -Name "DevTools" -PhysicalPath "C:\projects\insite-commerce\Src\DevTools\bin\Debug\net5.0\publish\" -BindingInformation "*:4000:" -PassThru
-$site.Applications["/"].ApplicationPoolName = "DevTools"
-Stop-IISCommitDelay
---
+++ tools/update.ps1
@@ -0,0 +1,22 @@
+# Update main branches
+#
+# Progression is dev -> cloud-rc -> cloud-release. Code in dev eventually ends up in cloud-rc and
+# then cloud-release; code in cloud-rc eventually ends up in cloud-release.
+Set-Location c:\projects\insite-commerce
+git checkout cloud-release
+git pull
+git checkout cloud-rc
+git pull
+git checkout dev
+git pull
+
+# Publish DevTools
+Set-Location c:\projects\insite-commerce\Src\Devtools
+dotnet publish
+
+Write-Output ""
+Write-Output "You still have to build in VS2022. Haven't figured that part out yet."
+
+# Build InsiteCommerce.Web
+# Set-Location c:\projects\insite-commerce\Legacy
+# dotnet build InsiteCommerce.sln
